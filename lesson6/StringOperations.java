601cca9f0bfaea1b84b8375d67c915e846b997e0
«Evgeniy» <jekson0702@gmail.com>
2019-12-04 14:54:40 +0300

    lesson6

---
+++ lesson6/StringOperations.java
@@ -0,0 +1,40 @@
+package pvt;
+
+public class StringOperations {
+
+    public int getSum(int a, int b) {
+        return a + b;
+    }
+
+    public double getSqrt(int a){
+        return Math.sqrt(a);
+    }
+
+    public int getDivision(int a, int b) {
+        return  a / b;
+    }
+
+    public double getAverage(int a, int b) {
+        return (double) (a + b) / 2;
+    }
+
+    public String getSumUpperCase(String a, String b) {
+        return (a + b).toUpperCase();
+    }
+
+    public String getReverseSumUpperCase(String a, String b) {
+        return (b + a).toUpperCase();
+    }
+    public String getSum(String line1, String line2) {
+        return line1 + line2;
+    }
+
+    public String getReverseSum(String line1, String line2, String line3) {
+        return line3 + line2 + line1;
+    }
+
+    public int getLength(String line) {
+        return line.length();
+    }
+
+}
